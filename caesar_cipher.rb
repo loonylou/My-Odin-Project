564a849082b88e192b6c02edb735a1a4ef4728d0
Louise <loonylou@gmail.com>
2024-02-01 00:44:53 +0000

    Update caesar_cipher.rb

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,6 +1,6 @@
-#Task
-#https://www.theodinproject.com/lessons/ruby-caesar-cipher
-#Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string:
+# Task
+# https://www.theodinproject.com/lessons/ruby-caesar-cipher
+# Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string:
 
 def caesar_cipher(text, shift)
   lower = ('a'..'z').to_a
