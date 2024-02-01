c61a779f4c9ad84861ee926c6767b235257dd3bf
Louise <loonylou@gmail.com>
2024-02-01 00:50:01 +0000

    Update substrings.rb

--- substrings.rb
+++ substrings.rb
@@ -1,4 +1,5 @@
 ### Task
+# https://www.theodinproject.com/lessons/ruby-sub-strings
 # Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. 
 # It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
 # Next, make sure your method can handle multiple words:
