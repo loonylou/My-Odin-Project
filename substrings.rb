9cc4cdbdefe514a79c037ca62180973ed2d42c11
Louise <loonylou@gmail.com>
2024-02-01 00:48:24 +0000

    Create substrings.rb

---
+++ substrings.rb
@@ -0,0 +1,14 @@
+### Task
+# Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. 
+# It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
+# Next, make sure your method can handle multiple words:
+
+### Test 1
+# dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# => ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# substrings("below", dictionary)
+# => { "below" => 1, "low" => 1 }
+
+### Test 2
+# > substrings("Howdy partner, sit down! How's it going?", dictionary)
+# => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
